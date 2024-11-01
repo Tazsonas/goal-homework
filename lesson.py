911369304a7f94c25667cd42c3b1e63589fa1d47
Tazsonas <tazokochadze2@gmail.com>
2024-11-01 19:12:24 +0400

    commit

---
+++ lesson.py
@@ -0,0 +1,34 @@
+points = 50
+
+points = 200
+
+points = 500
+print(points)
+
+
+#snake_case ვიყენებთ მაშინ როდესაც ერთზე მეტ სტყვას ვიყენებთ პითონში ერთმანეთისგან გამოვყოთ _ ის საშუალებით
+
+#name = "Tazo"
+#age = 9
+#lives = "GEORGIA"
+#surname = "kochadze"
+#username = "Tazsonas"
+#Name = deme
+#Age = 5
+#Lives = GEORGIA
+#Surname = kochadze
+#NAME = TAZO 
+#SURNAME = KOCHADZE
+
+
+
+number = 7
+age = 9
+print(7 + 9)
+
+
+print(20 + 50)
+print(50 - 20)
+print(50 * 20)
+print(50  / 20)
+
