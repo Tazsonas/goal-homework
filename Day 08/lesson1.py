116837809434d160d4fc72dcd84c6f2e3ad02443
Tazsonas <tazokochadze2@gmail.com>
2024-11-29 19:52:56 +0400

    commit

---
+++ Day 08/lesson1.py
@@ -0,0 +1,10 @@
+# ჩაწერე განსხვავებული მონაცემთა ტიპების (int, float, string, boolean) ცვლადები.
+number = 18
+height = 1.50
+name = "Tazo"
+boolean = True 
+
+print(type(number))
+print(type(height))
+print(type(name))
+print(type(boolean))
---
+++ Day 08/lesson4.py
@@ -0,0 +1,7 @@
+# მომხმარებელს შემოატანინეთ სახელი გვარი ასაკი სიმაღლე საყვარელი ფერი და გამოიტანეთ ეს ყველაფერი ერთ დიდ წინადადებაში.
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+height = input("enter your height: ")
+colour = input("enter your colour: ")
+print(name,  age, height, colour)
