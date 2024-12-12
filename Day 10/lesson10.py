1ccb9795239ec36acba5772776847219606cbb7c
Tazsonas <tazokochadze2@gmail.com>
2024-12-12 17:08:58 +0400

    second commit

---
+++ Day 10/lesson10.py
@@ -0,0 +1,26 @@
+# 1)ივარჯიშეთ შედარების ოპერატორებზე, გააკეთეთ 10-10 მაგალითი. კომენტარებით ახსენით, რა პასუხს გვიბრუნებენ შედარების ოპერატორები.
+
+print(456 + 546)#True
+print(240 + 630) #True
+print(799 + 900) #True
+print(777 + 9028) #True
+print(757 + 9087) #True
+print(6493 - 3673) #True
+print(3884 - 3394) #True
+print(6399 - 683) #True
+print(56777 - 2839)# True
+print(3873 - 89) #True
+print(7474 / 748) #True
+print(73794 / 9847) #True
+print(64746 / 7) #True
+print(634948 / 8) #True
+print(44385 / 847)  #True
+print(6736 * 77) #True
+print(907 * 789) #True
+print(790 * 93) #True
+print(700 * 99) #True
+print(778899 * 779) #False
+
+
+
+
---
+++ Day 10/lesson12.py
@@ -0,0 +1,11 @@
+# 3)მომხმარებელს შემოატანინეთ ორი რიცხვი და გაიგეთ მეტია თუ არა პირველი მეოორეზე
+age = 9
+height = 1.55
+age = input("enter your age"  )
+height = input("enter your height"  )
+print(age)
+print(height)
+print("age > height")
+print("age - height")
+
+
