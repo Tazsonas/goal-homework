5f148d8756f2c8273c01eee0a7804e34c35c0bf0
Tazsonas <tazokochadze2@gmail.com>
2025-01-10 18:38:53 +0400

    second commit

---
+++ Day 14/homework/homework 2) .py
@@ -0,0 +1,24 @@
+# 2)შეურიეთ შედარების და ლოგიკური ოპერატორები და გააკეთეთ ისევ 10 მაგალითი 
+
+print(66 > 56)
+print(67 < 56)
+print(657 > 467)
+print(78 < 68)
+print(6468 > 6666)
+print(656 > 787)
+print(True and False)
+print(False and False)
+print(True or False)
+print(True or True)
+
+
+
+
+
+
+
+
+
+
+
+
