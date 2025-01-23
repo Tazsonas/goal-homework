de0bcc4a625918bf5b76fa9401a5c7644d297972
Tazsonas <tazokochadze2@gmail.com>
2025-01-23 15:43:54 +0400

    second commit

---
+++ Day 16/lesson16.py/homework3.py
@@ -0,0 +1,26 @@
+print(568 > 457)
+print(9374 < 29)
+print(777> 555)
+print(7 > 1)
+print(8797 < 67)
+
+print(True and True)
+print(False and True)
+print(True or False)
+print(False or False)
+print(True or True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
