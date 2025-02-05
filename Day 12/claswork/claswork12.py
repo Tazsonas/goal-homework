e6a5dd64f39da2c93b973dfa96120881f42ebacd
Tazsonas <tazokochadze2@gmail.com>
2025-02-05 16:41:49 +0400

    commit

--- Day 12/claswork/claswork12.py
+++ Day 12/claswork/claswork12.py
@@ -1,2 +1,20 @@
-
-
+number1 = 7>968
+number2 = 676<5784
+number3 = 7878<230
+number4 = 7688>500
+number5 = 4778<700
+number6= 76>503
+number7 = 67>875
+number8 = 46<67
+number9 = 777>467
+number10 = 47877<777
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+print(number5)
+print(number6)
+print(number7)
+print(number8)
+print(number9)
+print(number10)
---
+++ Day 18/claswork/lesson18.py
@@ -0,0 +1,32 @@
+for i in range(0,10):
+   print(i)
+  
+
+for i in range(50,150):
+   print(i)
+
+
+for i in range(200,500,2):
+    print(i)
+
+
+
+for i in range(1,50,2):
+   print(i)
+
+
+input("enter your number 1")
+input("enter your number 2")
+input("enter your number 3")
+number = 7
+number = 10
+number = 19
+for i in range(7,19):
+    print(i)
+
+input("enter yor name")
+name = "Tazo"
+for i in range("Tazo"):
+       print(i)
+
+
